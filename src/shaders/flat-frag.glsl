03957eb5af8930172e332a40e5f2d488e444e30a
An Duong <onlyname@seas.upenn.edu>
2021-10-13 22:38:06 -0400

    done

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -1,7 +1,7 @@
 #version 300 es
 precision highp float;
 
-const int MAX_RAY_STEPS = 256;
+const int MAX_RAY_STEPS = 150;
 const float EPSILON = 1e-4;
 const float FOV = 60.0;
 const float MAX_DISTANCE = 20.0;
@@ -25,6 +25,12 @@ struct Intersection
     vec3 normal;
     float distance_t;
     int material_id;
+};
+
+struct DirectionalLight
+{
+    vec3 dir;
+    vec3 color;
 };
 
 // HELPERS //
@@ -78,15 +84,6 @@ mat4 rotateZ(float theta) {
     return inverse(m);
 }
 
-vec3 bendPoint(vec3 p, float k)
-{
-    float c = cos(k*p.y);
-    float s = sin(k*p.y);
-    mat2  m = mat2(c,-s,s,c);
-    vec3  q = vec3(m*p.xy,p.z);
-    return q;
-}
-
 float sdfSphere(vec3 query_position, vec3 position, float radius)
 {
     return length(query_position - position) - radius;
@@ -125,6 +122,12 @@ float smin( float a, float b, float k )
 }
 
 // from IQ
+vec3 applyFog(vec3 rgb, vec3 fogColor, float density, float dist)
+{
+    float fogAmount = 1.0 - exp( -dist*density );
+    return mix( rgb, fogColor, fogAmount );
+}
+
 vec3 opRep(vec3 p, vec3 c)
 {
     return mod(p+0.5*c,c)-0.5*c;
@@ -135,15 +138,6 @@ vec3 opRepLim(vec3 p, float c, vec3 l)
     return p-c*clamp(round(p/c),-l,l);
 }
 
-vec3 opTwist( vec3 p )
-{
-    const float k = 10.0; // or some other amount
-    float c = cos(k*p.y);
-    float s = sin(k*p.y);
-    mat2  m = mat2(c,-s,s,c);
-    return vec3(m*p.xz,p.y);
-}
-
 vec3 opRevolution( in vec3 p, float o )
 {
     return vec3( length(p.xz) - o, p.y , 0);
@@ -151,14 +145,25 @@ vec3 opRevolution( in vec3 p, float o )
 
 float pillar(vec3 pos, float rotation)
 {
-  return sdCylinder(pos, (rotateX(rotation) * vec4(0.57, -0.3, 0.0, 1.0)).xyz, 
-    (rotateX(rotation) * vec4(0.57, 0.3, 0.0, 1.0)).xyz, 0.02);
+  return sdCylinder(pos, (rotateX(rotation) * vec4(0.57, -2.0, 0.0, 1.0)).xyz, 
+    (rotateX(rotation) * vec4(0.57, 1.6, 0.0, 1.0)).xyz, 0.02);
 }
 
 float towerFloor(vec3 pos)
 {
+  return sdCylinder(pos, vec3(0.0, -0.05, 0.0), vec3(0.0, 0.05, 0.0), 0.64);
+}
+
+float towerBody(vec3 pos)
+{
+  vec3 displacement = vec3(0.0, 0.4, 0.0);
+  // base
+  float middle = sdCylinder(pos, vec3(0.0, -1.5, 0.0), vec3(0.0, 1.6, 0.0), 0.5);
+  // upper tower
+  float upper = sdCylinder(pos, vec3(0.0, 1.60, 0.0), vec3(0.0, 2.2, 0.0), 0.45);
+  middle = min(middle, upper);
+  // pillars
   float rotation = radians(45.0);
-  float tFloor = sdCylinder(pos, vec3(0.0, -0.05, 0.0), vec3(0.0, 0.05, 0.0), 0.64);
   float p1 = pillar(pos, 0.0);
   float p2 = pillar(pos, rotation * 1.0);
   float p3 = pillar(pos, rotation * 2.0);
@@ -167,49 +172,77 @@ float towerFloor(vec3 pos)
   float p6 = pillar(pos, rotation * 5.0);
   float p7 = pillar(pos, rotation * 6.0);
   float p8 = pillar(pos, rotation * 7.0);
-
-  return smin(tFloor, min(p1, min(p2, min(p3, min(p4, min(p5, min(p6, min(p7, p8))))))), 0.05);
-}
-
-float towerBody(vec3 pos)
-{
-  vec3 displacement = vec3(0.0, 0.4, 0.0);
-  float middle = sdCylinder(pos, vec3(0.0, -1.5, 0.0), vec3(0.0, 1.9, 0.0), 0.5);
-  float levels = towerFloor(opRepLim(pos, 0.5, vec3(0.0, 2.0, 0.0)));
-  float top = sdCylinder(pos + vec3(0.0, -1.3, 0.0), vec3(0.0, -0.05, 0.0), vec3(0.0, 0.05, 0.0), 0.64);
-  return smin(middle, min(levels, top), 0.05);
+  float pillars = min(p1, min(p2, min(p3, min(p4, min(p5, min(p6, min(p7, p8)))))));
+  // floors
+  float levels = towerFloor(opRepLim(pos + vec3(0.0, -0.1, 0.0), 0.5, vec3(0.0, 2.0, 0.0)));
+  float top = sdCylinder(pos + vec3(0.0, -1.6, 0.0), vec3(0.0, -0.05, 0.0), vec3(0.0, 0.05, 0.0), 0.64);
+  
+  return smin(middle, smin(min(levels, top), pillars, 0.05), 0.05);
 }
 
 float towerBase(vec3 pos)
 {
-  float middle = sdCylinder(pos, vec3(0.0, -0.5, 0.0), vec3(0.0, 0.5, 0.0), 0.64);
+  float middle = sdCylinder(pos, vec3(0.0, -0.5, 0.0), vec3(0.0, 0.45, 0.0), 0.64);
   return middle;
 }
 
-float tower(vec3 pos)
+float tower(vec3 pos, float minDist)
 {
+  // float bound = sdRoundBox(pos + vec3(0.0, 1.0, 0.0), vec3(3.0, 3.0, 3.0), 0.0);
+  // if (bound > minDist)
+  // {
+  //   return minDist;
+  // }
+
   float rotation = radians(-25.0) * (sin(u_Time / 50.0) + 1.0) / 2.0;
   vec3 rotatedPos = (rotateZ(rotation) * vec4(pos + vec3(0.0, 3.0, 0.0), 1.0)).xyz + vec3(0.0, -3.0, 0.0) + vec3(0.0, 0.2, 0.0);
-  return min(towerBody(rotatedPos), towerBase(rotatedPos + vec3(0.0, 1.8, 0.0)));
+  return smin(towerBody(rotatedPos), towerBase(rotatedPos + vec3(0.0, 1.8, 0.0)), 0.05);
 }
 
 float people(vec3 pos, float size)
 {
   vec3 movement = vec3(0.0, 0.08, 0.0) * triangleWave((u_Time + 3.5 * fs_Pos.x + 1.2 * fs_Pos.y) / 25.0, 10.0, 2.0);
-  float crowd = sdCylinder(opRepLim(pos + vec3(0.0, 2.4, 0.0) + movement, 0.25, vec3(size, 0.0, size)), vec3(0.0, 0.0,0.0), vec3(0.0,0.05,0.0), 0.02);
+  float crowd = sdCylinder(opRepLim(pos + vec3(0.0, 2.4, 0.0) + movement, 0.25, vec3(size, 0.0, size)), vec3(0.0, -0.05,0.0), vec3(0.0,0.05,0.0), 0.02);
   return crowd;
+}
+
+float crowds(vec3 pos)
+{
+  return min(people(pos + vec3(-1.3, 0.0, 1.2), 3.0), 
+             min(people(pos + vec3(2.0, 0.0, -2.4), 2.0), 
+                 people(pos + vec3(-1.5, 0.0, -1.5), 4.0)));
 }
 
 // SCENE //
 #define FLOOR plane(queryPos, -2.5)
-#define BODY tower(queryPos + vec3(0.0, 0.2, 0.0))
-#define THING min(people(queryPos + vec3(-1.3, 0.0, 1.2), 3.0), min(people(queryPos + vec3(2.0, 0.0, -2.4), 2.0), people(queryPos + vec3(-1.5, 0.0, -1.5), 4.0)))
+#define BODY tower(queryPos + vec3(0.0, 0.2, 0.0), dist)
+#define THING crowds(queryPos)
 
 #define FLOOR_NUM 0
 #define BODY_NUM 2
 #define THING_NUM 3
 
+// material colors //
+#define GRASS_COLOR rgb(vec3(10, 54, 21))
+#define TOWER_COLOR rgb(vec3(235, 242, 225))
+#define PEOPLE_COLOR rgb(vec3(88, 230, 232))
+
+// light colors //
+#define SUN_KEY_LIGHT rgb(vec3(186, 255, 237)) * 1.5
+#define SKY_FILL_LIGHT rgb(vec3(255, 233, 181)) * 0.2
+#define SUN_AMBIENT_LIGHT rgb(vec3(181, 178, 16)) * 0.2
+#define OUTSIDE_BACKGROUND rgb(vec3(49, 98, 173))
+#define OUTSIDE_BACKGROUND_2 rgb(vec3(121, 156, 209))
+
 float sceneSDF(vec3 queryPos)
+{
+  float dist = FLOOR;
+  dist = min(dist, BODY);
+  dist = min(dist, THING);
+  return dist;
+}
+
+float shadowSDF(vec3 queryPos)
 {
   float dist = FLOOR;
   dist = min(dist, BODY);
@@ -301,11 +334,12 @@ Intersection getRaymarchedIntersection(vec2 uv)
     return intersection;
 }
 
-float softShadow(vec3 origin, vec3 dir, float min_t, float max_t, float k) {
+float softShadow(vec3 origin, vec3 dir, float min_t, float k) {
     float res = 1.0;
-    for(float t = min_t; t < max_t;) 
+    float t = min_t;
+    for(int i = 0; i < MAX_RAY_STEPS; ++i) 
     {
-        float h = sceneSDF(origin + t * dir);
+        float h = shadowSDF(origin + t * dir);
         if (h < EPSILON) 
         {
             return 0.0;
@@ -316,41 +350,62 @@ float softShadow(vec3 origin, vec3 dir, float min_t, float max_t, float k) {
     return res;
 }
 
-vec3 calculateMaterial(int material_id, vec3 normal, vec3 lightDir, vec3 viewDir)
+vec3 calculateMaterial(int material_id, vec3 p, vec3 n, vec3 viewDir)
 {
+  DirectionalLight lights[3];
+  lights[0] = DirectionalLight(normalize(vec3(-10.0, 15.0, -10.0)),
+                               SUN_KEY_LIGHT);
+  lights[1] = DirectionalLight(vec3(0., 1., 0.),
+                               SKY_FILL_LIGHT);
+  lights[2] = DirectionalLight(normalize(-vec3(15.0, 0.0, 10.0)),
+                               SUN_AMBIENT_LIGHT);
+  vec3 backgroundColor = mix(OUTSIDE_BACKGROUND_2, OUTSIDE_BACKGROUND, (fs_Pos.y + 1.0) / 2.0);
+
   float ambient = 0.2;
-  float lambert = max(0.0, dot(normal, lightDir)) + ambient;
-  vec3 halfVec = (lightDir + viewDir) / 2.0; 
-  float specular = pow(max(dot(halfVec, normal), 0.0), 32.0);
+  float lambert = max(0.0, dot(n, lights[0].dir)) + ambient;
+  vec3 halfVec = (lights[0].dir + viewDir) / 2.0; 
+  float specular = pow(max(dot(halfVec, n), 0.0), 32.0);
   float blinn = lambert + 0.75 * specular;
 
+  vec3 albedo;
   switch (material_id)
   {
     case FLOOR_NUM:
-      return rgb(vec3(82, 143, 47)) * blinn;
+      albedo = GRASS_COLOR;
       break;
     case BODY_NUM:
-      return rgb(vec3(226, 227, 200)) * blinn;
+      albedo = TOWER_COLOR;
       break;
     case THING_NUM:
-      return rgb(vec3(88, 230, 232)) * blinn;
+      albedo = PEOPLE_COLOR;
       break;
     case -1:
-      return rgb(vec3(149, 228, 252));
-      break;
+      return backgroundColor;
   }
-  return rgb(vec3(153, 139, 83));
+
+  vec3 color = albedo *
+               lights[0].color *
+               max(0.0, dot(n, lights[0].dir)) *
+               softShadow(p, lights[0].dir, 0.1, 12.0);
+  for(int i = 1; i < 3; ++i) 
+  {
+    color += albedo *
+             lights[i].color *
+             max(0.0, dot(n, lights[i].dir));
+  }
+  color *= blinn;
+  // gamma
+  color = pow(color, vec3(1. / 2.2));
+
+  return color;
 }
 
-vec3 getSceneColor(vec2 uv, vec3 lightPos)
+vec3 getSceneColor(vec2 uv, vec3 lightPos, out vec3 pos)
 {
   Intersection intersection = getRaymarchedIntersection(uv);
-  vec3 lightDir = normalize(lightPos - intersection.position);
-  vec3 light_t = (lightPos - intersection.position) / lightDir;
   vec3 viewDir = normalize(u_Ref - u_Eye);
-  float softShadow = softShadow(intersection.position, lightDir, 0.1, light_t.x, 12.0);
-  vec3 col = calculateMaterial(intersection.material_id, intersection.normal, lightDir, viewDir);
-  return col * softShadow;
+  pos = intersection.position;
+  return calculateMaterial(intersection.material_id, intersection.position, intersection.normal, viewDir);
 }
 
 void main() {
@@ -358,7 +413,14 @@ void main() {
 
   vec2 uv = fs_Pos;
 
-  vec3 col = getSceneColor(uv, lightPos);
+  vec3 pos;
+  vec3 col = getSceneColor(uv, lightPos, pos);
+
+  vec3 backgroundColor = rgb(vec3(23, 53, 99));
+
+  float fog = smoothstep(14.0, 20.0, distance(pos, u_Eye.xyz));
+
+  col = mix(col, backgroundColor, fog);
 
   out_Col = vec4(col, 1.0);
 }
